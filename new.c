c65d27340008bfcc1bf6c96cab0a161f8d9605f5
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2023-09-09 00:17:15 +0530

    .

--- new.c
+++ new.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    printf("hello world");
-    return 0;
+    int num1,num2,sum;
+    printf("Enter 1st number:");
+    scanf("%d",&num1);
+    printf("Enter 2nd number:");
+    scanf("%d",&num2);
+    sum = num1+num2;
+    printf("The Sum of the numbers is %d",sum);
+
 }
