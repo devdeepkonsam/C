84fdbb9e7e0f3e4674dad151fe8e3185fd7207e4
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2023-09-23 12:48:23 +0530

    .\

---
+++ BASIC_C4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    //An array is a data structure that stores a collection of related values that are all the same type.
+    //An array declaration includes the type of the values it stores, an identifier,
+    // and square brackets [ ] with a number that indicates the array size. 
+
+    int test_score[5] = {1,2,3,4,5};
+
+}
