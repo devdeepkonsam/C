b032d463e3fbd766647daaeba35c0d2fc6bfab97
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2023-11-14 10:37:38 +0530

    .

---
+++ src/LENGTH.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    int i;
+    printf("Enter a string: ");
+    scanf("%s",string);
+    for(i=0:str[i]!='\0':i++){
+    }
+    printf("size of the string is %d",i);
+    
+    return 0;
+}
