b9b208c8f2ab1c007de506d502e5b148227162f1
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2023-12-13 15:49:13 +0530

    New memory.c

--- src/memory.c
+++ src/memory.c
@@ -7,7 +7,8 @@ int main(void){
     printf("%d\n",n); //it print the value assigned to n 
     printf("%p\n",&n); //it print the address of the n 
     printf("%p\n",p); //here it will print the address of the n as the p is pointer which is assigned with the address of n using &n
-    printf("%i\n",*p); //here it will print the value save in the address *p ;
+    printf("%i\n",*p); //here it will print the value save in the address *p 
+    
     printf("%i\n",*&n); //it will print the value of n
     
 
